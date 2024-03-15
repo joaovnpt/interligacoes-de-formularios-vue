d0dd32a2306693a07c4db225bc9b3136baa2fa0f
joaovnpt <joaovitorguiotti8@gmail.com>
2024-03-15 16:51:27 -0300

    .

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,9 @@ const texto = ref('')
 <template>
   <div class="container">
     <input type="text" v-bind:value="texto" v-on:input="(event) => (texto = event.target.value)" />
-    //*v-bind* e *v-on* pode ser substitúido por ":" e "@"
-    //@ e : JUNTOS podem ser substituídos por "v-model"
-    //O input a cima poderia ser <input type="text" v-model="texto" />
+    <!-- *v-bind* e *v-on* pode ser substitúido por ":" e "@" 
+    @ e : JUNTOS podem ser substituídos por "v-model"
+    O input a cima poderia ser <input type="text" v-model="texto" /> -->
     <p>Texto digitado: {{ texto }}</p>
   </div>
 </template>
