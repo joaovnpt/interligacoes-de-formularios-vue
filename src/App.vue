2d613fb50bed0eadfa185f69481fbd6e22665d11
joaovnpt <joaovitorguiotti8@gmail.com>
2024-03-22 16:48:05 -0300

    Começado Projeto 1

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,35 @@ import { ref } from 'vue'
 const name = ref(''); //tex; minlength; required;
 const email = ref(''); //email; required;
 const age = ref(''); //number; required;
-const submit = ref('');
-let msg = ref('')
+const password = ref('');
+const confirmPassword = ref('');
+let msg = ref('');
+let submited = false;
 
-function submitForm() {
-  if (name.value.length !== 0 && age.value.length !== 0 && email.value.length !== 0 && age.value >= 18 && age.value <= 60) {
-    msg.value = `Form submitted sucessfully! Confirm your information Name: ${name.value} Age: ${age.value} E-mail: ${email.value}`;
+function clearForm() {
+  if (submited == true) {
+    name.value = '';
+    email.value = '';
+    age.value = '';
+    password.value = '';
+    confirmPassword.value = '';
   }
-  else alert('There was an error, fill in your informations and try again.')
 }
 
+function submitForm() {
+  submited = true
+  if (
+    name.value.length !== 0 &&
+    age.value.length !== 0 &&
+    email.value.length !== 0 &&
+    age.value >= 18 &&
+    age.value <= 60 &&
+    password.value === confirmPassword.value
+  ) {
+    msg.value = `Form submitted sucessfully!`;
+    clearForm()
+  } else alert('There was an error, fill in your informations and try again.');
+}
 </script>
 
 <template>
@@ -21,27 +40,37 @@ function submitForm() {
     <div class="msg-container" v-show="msg.length != 0">
       <p>{{ msg }}</p>
     </div>
-    <form class="vue-form" @submit.prevent="submitForm"> <!--@submit.prevent faz com que ao submitar, o navegador siga as instruções da função prescrita, e não recarregue a página e mande para o servidor (padrão)-->
+    <form class="vue-form" @submit.prevent="submitForm">
+      <!--@submit.prevent faz com que ao submitar, o navegador siga as instruções da função prescrita, e não recarregue a página e mande para o servidor (padrão)-->
       <div class="input-container">
         <label for="name">Name:</label>
-        <hr>
-        <input type="text" id="name" name="name" v-model="name" placeholder="Your name" minlength="3" maxlength="20" required>
-        <hr>
+        <hr />
+        <input type="text" id="name" name="name" v-model="name" placeholder="Your name" minlength="3" maxlength="20"
+          required />
+        <hr />
         <label for="email">E-mail</label>
-        <hr>
-        <input type="email" id="email" name="email" v-model="email" placeholder="Your best e-mail" required>
-        <hr>
+        <hr />
+        <input type="email" id="email" name="email" v-model="email" placeholder="Your best e-mail" required />
+        <hr />
+        <label for="password">Password</label>
+        <hr />
+        <input type="password" name="password" id="password" v-model="password" placeholder="Your best password"
+          minlength="8" />
+        <hr />
+        <label for="confirmPassword">Confirm password</label>
+        <hr />
+        <input type="password" name="confirmPassword" id="confirmPassword" v-model="confirmPassword"
+          placeholder="Confirm your password">
+        <hr />
         <label for="age">Age</label>
-        <hr>
-        <input type="number" name="age" id="age" v-model="age" placeholder="Your age" min="18" max="60" required>
+        <hr />
+        <input type="number" name="age" id="age" v-model="age" placeholder="Your age" min="18" max="60" required />
       </div>
       <div class="input-container">
-        <input type="submit" id="submit" value="Submit" v-model="submit" @click="submitForm()">
+        <button type="submit">Submit</button>
       </div>
     </form>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
