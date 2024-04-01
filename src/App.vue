b3dfe1bb3919d7b36b5c8a8eafc122a232a31fdf
joaovnpt <joaovitorguiotti8@gmail.com>
2024-04-01 15:13:53 -0300

    finalizado

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,75 @@
 <script setup>
 import { ref, reactive } from 'vue'
 
-const titulo = ref('Interligação de Formulários')
-const user = reactive({
-  name: '',
-  email: '',
-  age: '',
-  password: '',
-  confirmPassword: ''
-})
-
-let msg = ref('');
+const titulo = ref('Interligação de Formulários');
 let showResult = ref(false);
+const categories = [{
+  id: 1, name: 'Jewelry'
+},
+{
+  id: 2, name: 'Electronics'
+},
+{
+  id: 3, name: 'Clothes'
+},
+{
+  id: 4, name: 'Sports'
+}
+]
+//const categories = ['Jewelry', 'Electronics', 'Clothes', 'Sports']
+const product = reactive({
+  name: '',
+  price: '',
+  categories: [],
+  stock: ''
+})
+function submitForm() {
+  if (product.qtd < 0) {
+    alert('Stock must be positive.');
+    return;
+  }
+  showResult.value = !showResult.value
+}
 
-/* function submitForm() {
-  submited = true
-  if (
-    user.value.length !== 0 &&
-    user.age.value.length !== 0 &&
-    user.email.value.length !== 0 &&
-    user.age.value >= 18 &&
-    user.age.value <= 60 &&
-    user.password.value === user.confirmPassword.value
-  ) {
-    msg.value = `Form submitted sucessfully!`;
-    clearForm()
-  } else alert('There was an error, fill in your informations and try again.');
-} */
 </script>
 
 <template>
   <h1 class="titulo">{{ titulo }}</h1>
   <div class="container">
     <div class="form-container">
-      <div class="msg-container" v-show="msg.length != 0">
-        <p>{{ msg }}</p>
-      </div>
-      <form class="vue-form">
-        <!--@submit.prevent faz com que ao submitar, o navegador siga as instruções da função prescrita, e não recarregue a página e mande para o servidor (padrão)-->
+      <form class="vue-form" @submit.prevent="submitForm()">
         <div class="input-container">
           <label for="name">Name:</label>
-          <hr />
-          <input type="text" id="name" name="name" v-model="user.name" placeholder="Your name" minlength="3"
-            maxlength="20" required />
-          <hr />
-          <label for="age">Age</label>
-          <hr />
-          <input type="number" name="age" id="age" v-model="user.age" placeholder="Your age" min="18" max="60"
-            required />
-          <hr>
-          <label for="email">E-mail</label>
-          <hr />
-          <input type="email" id="email" name="email" v-model="user.email" placeholder="Your best e-mail" required />
-          <hr />
-          <label for="password">Password</label>
-          <hr />
-          <input type="password" name="password" id="password" v-model="user.password" placeholder="Your best password"
-            minlength="8" />
-          <hr />
-          <label for="confirmPassword">Confirm password</label>
-          <hr />
-          <input type="password" name="confirmPassword" id="confirmPassword" v-model="user.confirmPassword"
-            placeholder="Confirm your password">
+          <input type="text" id="name" name="name" v-model="product.name" placeholder="Product name" minlength="3"
+            maxlength="20" />
+          <label for="price">Price</label>
+          <input type="number" name="price" id="price" v-model="product.price" placeholder="Product price" />
+          <label for="category">Category</label>
+          <input type="category" id="category" name="category" v-model="product.category"
+            placeholder="Product category" />
+          <label for="stock">Stock</label>
+          <input type="stock" name="stock" id="stock" v-model="product.stock" placeholder="Stock" />
           <div class="input-container">
-            <button class="submit-btn" @click="showResult = !showResult">Submit</button>
+            <fieldset>
+              <legend>Categories:</legend>
+
+              <div v-for="category in categories" :key="category.id">
+                <input type="checkbox" v-model="product.categories" :value="category.id"> {{ category.name }}
+              </div>
+
+            </fieldset>
+
+            <button type="submit" class="submit-btn">Show data</button>
           </div>
         </div>
       </form>
     </div>
     <div class="resultado-container" v-if="showResult == true">
-      <h3>User data:</h3>
-      <p>Name: {{ user.name }}</p>
-      <p>Age: {{ user.age }}</p>
-      <p>E-mail: {{ user.email }}</p>
-      <p>Password: {{ user.password }}</p>
+      <h3>Product data:</h3>
+      <p>Name: {{ product.name }}</p>
+      <p>Price: {{ product.price }}</p>
+      <p>Category: {{ product.categories }}</p>
+      <p>Stock: {{ product.stock }}</p>
       <p>{{ showResult }}</p>
     </div>
   </div>
@@ -88,6 +84,13 @@ let showResult = ref(false);
   display: flex;
   flex-direction: row;
   column-gap: 2rem;
+}
+
+.input-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
 }
 
 .form-container,
@@ -110,5 +113,6 @@ let showResult = ref(false);
 
 .submit-btn {
   position: relative;
+  margin-top: 20px;
 }
 </style>
